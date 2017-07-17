f9d7d8597d7032e0c541f775a0c8fc5996740923
Joyce Chau <jyc.chau@gmail.com>
2017-07-16 18:43:15 -0700

    Setup project

---
+++ app.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      name: "",
+      cardNumber: "",
+      cvv2: "",
+      expMonth: "",
+      expYear: ""
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this);
+  }
+
+  update(field) {
+    return (e) => this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        className="form"
+      >
+        <input
+          type="text"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.update("name")}
+        />
+        <input
+          type="number"
+          placeholder="Card Number"
+          value={this.state.cardNumber}
+          onChange={this.update("cardNumber")}
+        />
+        <input
+          type="number"
+          placeholder="CVV2"
+          value={this.state.cvv2}
+          onChange={this.update("cvv2")}
+        />
+        <div>
+          <input
+            type="number"
+            placeholder="Exp. Month"
+            value={this.state.expMonth}
+            onChange={this.update("expMonth")}
+          />
+          <input
+            type="number"
+            placeholder="Exp. Year"
+            value={this.state.expYear}
+            onChange={this.update("expYear")}
+          />
+        </div>
+        <div>
+          <img/>
+          <img/>
+        </div>
+        <input
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+  ReactDOM.render(<App />, app);
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+    ]
+  },
+  devtool: 'source-maps'
+};
